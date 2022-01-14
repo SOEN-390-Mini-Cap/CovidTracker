e6904b6adf8a6e232433f06c62347081b411c898
Jason Gerard <jasongerard321@gmail.com>
2022-01-14 13:41:44 -0500

    COV-3: setup basic web server (#4)
    * added basic config files for a typescript project
    
    * added restify and inversify to project
    
    * updated folder structure
    
    * added eslint and prettier to ts project
    
    * cleaned up extra comment

---
+++ server/src/controllers/BaseController.ts
@@ -0,0 +1,14 @@
+import * as restify from "restify";
+import { Controller, Get, interfaces } from "inversify-restify-utils";
+import { injectable, inject } from "inversify";
+
+@Controller("/")
+@injectable()
+export class BaseController implements interfaces.Controller {
+    @Get("/")
+    private index(req: restify.Request, res: restify.Response): void {
+        res.json(200, {
+            status: "ok",
+        });
+    }
+}
---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { Container } from "inversify";
+import { interfaces, InversifyRestifyServer, TYPE } from "inversify-restify-utils";
+import { BaseController } from "./controllers/BaseController";
+
+const container = new Container();
+container.bind<interfaces.Controller>(TYPE.Controller).to(BaseController).whenTargetNamed("BaseController");
+
+const server = new InversifyRestifyServer(container);
+
+const app = server.build();
+
+app.listen(3000, () => {
+    console.log("Server has started on port 3000");
+});
