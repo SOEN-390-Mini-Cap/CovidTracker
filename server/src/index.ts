547dd272e3a2652f7e7a6ef22be7952f7e2e6db5
Jason Gerard <jasongerard321@gmail.com>
2022-01-16 00:40:37 -0500

    added other pipeline steps

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,6 @@ const server = new InversifyRestifyServer(container);
 
 const app = server.build();
 
-app.listen(3000, () =>
+app.listen(3000, () => {
     console.log("Server has started on port 3000");
 });
