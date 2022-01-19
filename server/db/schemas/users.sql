e084ea4337396e370d8861368f8b7c66b7a837f9
Jason Gerard <jasongerard321@gmail.com>
2022-01-18 22:43:06 -0500

    added basic db seed and table schema

---
+++ server/db/schemas/users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth TIMESTAMP NOT NULL,
+    created_on TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
---
+++ server/db/seed_data/UserData.ts
@@ -0,0 +1,16 @@
+export const users = [
+    {
+        email: "test@test.com",
+        password: "test123",
+        first_name: "john",
+        last_name: "smith",
+        date_of_birth: "2000-01-19 02:26:39.131000 +00:00",
+    },
+    {
+        email: "test2@test.com",
+        password: "test123",
+        first_name: "john2",
+        last_name: "smith",
+        date_of_birth: "2001-01-19 02:26:39.131000 +00:00",
+    }
+];
