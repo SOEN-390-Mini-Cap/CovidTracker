5a2cca1d3f0057940b533933f56374b58cb012eb
Jason Gerard <jasongerard321@gmail.com>
2022-01-27 19:36:30 -0500

    COV-50: build front end sign in page (#20)
    * added new files and removed extras
    
    * added basic libs
    
    * add login page
    
    * added styling for login page
    
    * updated eslint and added prettier
    
    * fix compile errors and update some styling
    
    * added default val for login controller
    
    * integrate login with backend
    
    * refactored login routing
    
    * compress libs
    
    * remove eslint check

---
+++ client/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+        es6: true,
+        browser: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    extends: ["react-app", "eslint:recommended", "plugin:prettier/recommended"],
+    ignorePatterns: ["libs/", "src/@core"],
+};
---
+++ client/src/router/routes/index.js
@@ -0,0 +1,27 @@
+import AppRoutes from "./Apps";
+import FormRoutes from "./Forms";
+import PagesRoutes from "./Pages";
+import TablesRoutes from "./Tables";
+import ChartsRoutes from "./Charts";
+import DashboardRoutes from "./Dashboards";
+import UiElementRoutes from "./UiElements";
+import ExtensionsRoutes from "./Extensions";
+import PageLayoutsRoutes from "./PageLayouts";
+
+// ** Default Route
+const DefaultRoute = "/home";
+
+// ** Merge Routes
+const Routes = [
+    ...DashboardRoutes,
+    ...AppRoutes,
+    ...PagesRoutes,
+    ...UiElementRoutes,
+    ...ExtensionsRoutes,
+    ...PageLayoutsRoutes,
+    ...FormRoutes,
+    ...TablesRoutes,
+    ...ChartsRoutes,
+];
+
+export { DefaultRoute, Routes };
