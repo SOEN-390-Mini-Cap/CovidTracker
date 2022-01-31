b778f908397c7b8262c2b2e8926f8598b5b659a5
Jason Gerard <jasongerard321@gmail.com>
2022-01-30 23:09:29 -0500

    remove extra edits

--- client/src/views/forms/wizard/steps-with-validation/AccountDetails.js
+++ client/src/views/forms/wizard/steps-with-validation/AccountDetails.js
@@ -121,12 +121,13 @@ const AccountDetails = ({ stepper }) => {
                     </div>
                 </Row>
                 <div className="d-flex justify-content-between">
-                    <Button color="primary" className="btn-prev" onClick={() => stepper.previous()}>
+                    <Button color="secondary" className="btn-prev" outline disabled>
                         <ArrowLeft size={14} className="align-middle me-sm-25 me-0"></ArrowLeft>
                         <span className="align-middle d-sm-inline-block d-none">Previous</span>
                     </Button>
-                    <Button type="submit" color="success" className="btn-submit">
-                        Submit
+                    <Button type="submit" color="primary" className="btn-next">
+                        <span className="align-middle d-sm-inline-block d-none">Next</span>
+                        <ArrowRight size={14} className="align-middle ms-sm-25 ms-0"></ArrowRight>
                     </Button>
                 </div>
             </Form>
--- client/src/views/forms/wizard/steps/AccountDetails.js
+++ client/src/views/forms/wizard/steps/AccountDetails.js
@@ -50,12 +50,13 @@ const AccountDetails = ({ stepper, type }) => {
                     </div>
                 </Row>
                 <div className="d-flex justify-content-between">
-                    <Button color="primary" className="btn-prev" onClick={() => stepper.previous()}>
+                    <Button color="secondary" className="btn-prev" outline disabled>
                         <ArrowLeft size={14} className="align-middle me-sm-25 me-0"></ArrowLeft>
                         <span className="align-middle d-sm-inline-block d-none">Previous</span>
                     </Button>
-                    <Button type="submit" color="success" className="btn-submit">
-                        Submit
+                    <Button color="primary" className="btn-next" onClick={() => stepper.next()}>
+                        <span className="align-middle d-sm-inline-block d-none">Next</span>
+                        <ArrowRight size={14} className="align-middle ms-sm-25 ms-0"></ArrowRight>
                     </Button>
                 </div>
             </Form>
