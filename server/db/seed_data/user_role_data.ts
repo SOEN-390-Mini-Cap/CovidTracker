f9e4beba01db8a881f49c5a9914f39ff5f663a39
Jason Gerard <jasongerard321@gmail.com>
2022-02-05 10:52:53 -0500

    fix db seed script with new default user roles

--- server/db/seed_data/user_role_data.ts
+++ server/db/seed_data/user_role_data.ts
@@ -1,18 +1,18 @@
 export const userRoles = [
     {
         user_id: 1,
-        role_id: 1,
-    },
-    {
-        user_id: 1,
         role_id: 2,
     },
     {
+        user_id: 1,
+        role_id: 3,
+    },
+    {
         user_id: 2,
-        role_id: 1,
+        role_id: 2,
     },
     {
         user_id: 3,
-        role_id: 1,
+        role_id: 5,
     },
 ];
