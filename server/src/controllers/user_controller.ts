3522a003c6649b720033ffce2b0a4ece7af6e871
Andre <psnmeee@gmail.com>
2022-02-06 20:34:13 -0500

    added access right middleware to be tested

--- server/src/controllers/user_controller.ts
+++ server/src/controllers/user_controller.ts
@@ -2,8 +2,9 @@ import "reflect-metadata";
 import { Request, Response } from "restify";
 import { Controller, Get, interfaces } from "inversify-restify-utils";
 import { inject, injectable, named } from "inversify";
-import { extractJWTAuthMiddleware } from "./auth_utils";
 import { UserService } from "../services/user_service";
+import { extractJWTAuthMiddleware, accessRightsMiddleware } from "./authMiddleware";
+import { Role } from "../entities/role";
 
 @Controller("/users")
 @injectable()
@@ -13,8 +14,8 @@ export class UserController implements interfaces.Controller {
         @named("UserService")
         private readonly userService: UserService,
     ) {}
-
-    @Get("/me", extractJWTAuthMiddleware)
+    //
+    @Get("/me", extractJWTAuthMiddleware, accessRightsMiddleware([Role.USER]))
     private async me(req: Request, res: Response): Promise<void> {
         try {
             const userId = req["token"].userId;
