bcedcbac77134d3613ca852f68e767cfe1e67203
Jason Gerard <jasongerard321@gmail.com>
2022-02-08 14:31:51 -0500

    remove extra auth pages

--- client/src/router/routes/Pages.js
+++ client/src/router/routes/Pages.js
@@ -22,54 +22,6 @@ const PagesRoutes = [
         },
     },
     {
-        path: "/forgot-password",
-        component: lazy(() => import("../../views/pages/authentication/ForgotPassword")),
-        layout: "BlankLayout",
-        meta: {
-            authRoute: true,
-        },
-    },
-    {
-        path: "/pages/forgot-password-basic",
-        component: lazy(() => import("../../views/pages/authentication/ForgotPasswordBasic")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/forgot-password-cover",
-        component: lazy(() => import("../../views/pages/authentication/ForgotPasswordCover.js")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/reset-password-basic",
-        component: lazy(() => import("../../views/pages/authentication/ResetPasswordBasic")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/reset-password-cover",
-        component: lazy(() => import("../../views/pages/authentication/ResetPasswordCover")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/verify-email-basic",
-        component: lazy(() => import("../../views/pages/authentication/VerifyEmailBasic")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/verify-email-cover",
-        component: lazy(() => import("../../views/pages/authentication/VerifyEmailCover")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/two-steps-basic",
-        component: lazy(() => import("../../views/pages/authentication/TwoStepsBasic")),
-        layout: "BlankLayout",
-    },
-    {
-        path: "/pages/two-steps-cover",
-        component: lazy(() => import("../../views/pages/authentication/TwoStepsCover")),
-        layout: "BlankLayout",
-    },
-    {
         path: "/pages/profile",
         component: lazy(() => import("../../views/pages/profile")),
     },
@@ -85,7 +37,6 @@ const PagesRoutes = [
         path: "/pages/api-key",
         component: lazy(() => import("../../views/pages/api-key")),
     },
-
     {
         path: "/misc/coming-soon",
         component: lazy(() => import("../../views/pages/misc/ComingSoon")),
