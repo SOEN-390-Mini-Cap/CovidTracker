bbab13049ba0058ddc42e5fededaa87451ad9255
Jason Gerard <jasongerard321@gmail.com>
2022-02-08 00:40:36 -0500

    updated repositories

--- server/db/schemas/roles.sql
+++ server/db/schemas/roles.sql
@@ -1,5 +1,12 @@
 CREATE TABLE roles (
-    role_id SERIAL,
-    role_name VARCHAR (255) UNIQUE NOT NULL,
+    role_id INT,
+    role_name VARCHAR(255) UNIQUE NOT NULL,
     PRIMARY KEY (role_id)
 );
+
+INSERT INTO roles (role_id, role_name) VALUES (1, 'USER');
+INSERT INTO roles (role_id, role_name) VALUES (2, 'PATIENT');
+INSERT INTO roles (role_id, role_name) VALUES (3, 'DOCTOR');
+INSERT INTO roles (role_id, role_name) VALUES (4, 'ADMIN');
+INSERT INTO roles (role_id, role_name) VALUES (5, 'HEALTH_OFFICIAL');
+INSERT INTO roles (role_id, role_name) VALUES (6, 'IMMIGRATION_OFFICER');
