9482a0df0e3b3f4e75e4cc59598344959f780561
Jason Gerard <jasongerard321@gmail.com>
2022-02-08 14:56:29 -0500

    refactor route auth function

--- client/src/views/pages/authentication/SignIn.js
+++ client/src/views/pages/authentication/SignIn.js
@@ -3,7 +3,6 @@ import { useDispatch } from "react-redux";
 import { useForm, Controller } from "react-hook-form";
 import { handleLogin } from "@store/authentication";
 import InputPasswordToggle from "@components/input-password-toggle";
-import { getHomeRouteForLoggedInUser } from "@utils";
 import { Form, Input, Label, Button, Card, CardBody, FormFeedback } from "reactstrap";
 import "@styles/react/pages/page-authentication.scss";
 import axios from "axios";
@@ -65,7 +64,7 @@ const SignIn = () => {
                     rememberMe: data.rememberMe,
                 }),
             );
-            history.push(getHomeRouteForLoggedInUser("admin"));
+            history.push("/");
         } catch (error) {
             setError("req", {
                 type: "manual",
