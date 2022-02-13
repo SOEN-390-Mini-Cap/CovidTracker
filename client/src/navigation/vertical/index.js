564c6eeb57f65160b790b2bafaedb206bf925c3a
Jason Gerard <jasongerard321@gmail.com>
2022-02-13 18:17:27 -0500

    update navbar visibility by role

--- client/src/navigation/vertical/index.js
+++ client/src/navigation/vertical/index.js
@@ -6,17 +6,20 @@ export default [
         title: "Dashboard",
         icon: <Home />,
         navLink: "/home",
+        accessibleBy: ["USER", "PATIENT", "DOCTOR", "HEALTH_OFFICIAL", "IMMIGRATION_OFFICER", "ADMIN"],
     },
     {
         id: "doctor",
         title: "Doctor",
         icon: <Activity />,
+        accessibleBy: ["ADMIN"],
         children: [
             {
                 id: "patientsAssigned",
                 title: "Patients Assigned",
                 icon: <Circle />,
                 navLink: "/patients_assigned",
+                accessibleBy: ["ADMIN"],
             },
         ],
     },
