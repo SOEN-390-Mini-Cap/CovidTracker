28a83246dab7e8f7d0930d8d088c72bc61bb6b14
Jason Gerard <jasongerard321@gmail.com>
2022-02-28 16:21:56 -0500

    added other seed fields

--- server/db/seed_db.ts
+++ server/db/seed_db.ts
@@ -10,25 +10,28 @@ import { AdminRepository } from "../src/repositories/admin_repository";
 import { StatusRepository } from "../src/repositories/status_repository";
 import { HealthOfficialRepository } from "../src/repositories/health_official_repository";
 import faker from "@faker-js/faker";
-import {Address} from "../src/entities/address";
+import { Address } from "../src/entities/address";
 import {User} from "../src/entities/user";
 import {Gender} from "../src/entities/gender";
 import {RequestUser} from "../src/entities/request/RequestUser";
+import {ImmigrationOfficerRepository} from "../src/repositories/immigration_officer_repository";
+import { sampleSymptoms } from "./seed_data/sample_symptoms";
 
 export async function seedDb(): Promise<void> {
-    // const pool = new Pool();
-    // const userRepository = new UserRepository(pool);
-    // const doctorRepository = new DoctorRepository(pool, userRepository);
-    // const patientRepository = new PatientRepository(pool, userRepository);
-    // const adminRepository = new AdminRepository(pool, userRepository);
-    // const statusRepository = new StatusRepository(pool);
-    // const healthOfficialRepository = new HealthOfficialRepository(pool, userRepository);
+    const pool = new Pool();
+    const userRepository = new UserRepository(pool);
+    const doctorRepository = new DoctorRepository(pool, userRepository);
+    const patientRepository = new PatientRepository(pool, userRepository);
+    const adminRepository = new AdminRepository(pool, userRepository);
+    const statusRepository = new StatusRepository(pool);
+    const healthOfficialRepository = new HealthOfficialRepository(pool, userRepository);
+    const immigrationOfficerRepository = new ImmigrationOfficerRepository(pool, userRepository);
 
     faker.setLocale("en_CA");
 
-    const numAddresses = 20;
     // insert addresses
-    for (let i = 0; i < numAddresses; i++) {
+    const numAddresses = [1, 3];
+    for (let i = numAddresses[0]; i < numAddresses[1]; i++) {
         const isEveryFifth = i % 5 === 0;
 
         const address: Address = {
@@ -41,16 +44,19 @@ export async function seedDb(): Promise<void> {
             country: "Canada",
         };
 
-        // await userRepository.addAddress(address);
+        await userRepository.addAddress(address);
     }
 
-    const numPatients = 900;
-    const numDoctors = 90;
-    const numAdmins = 1;
-    const numHealthOfficials = 1;
-    const numImmigrationOfficers = 1;
+    // 50 patients per address
+    const numPatients = [1, 50 * (numAddresses[1] - 1) + 1];
+    // 20 patients per doctor
+    const numDoctors = [numPatients[1], numPatients[1] + (numPatients[1] - 1) / 20];
+    const numAdmins = [numDoctors[1], numDoctors[1] + 1];
+    const numHealthOfficials = [numAdmins[1], numAdmins[1] + 1];
+    const numImmigrationOfficers = [numHealthOfficials[1], numHealthOfficials[1] + 1];
 
-    const numUsers = numPatients + numDoctors + numAdmins + numHealthOfficials + numImmigrationOfficers;
+    // insert users
+    const numUsers = numImmigrationOfficers[1];
     for (let i = 0; i < numUsers; i++) {
         const rawGender = faker.name.gender(true);
         const firstName = faker.name.firstName(rawGender);
@@ -66,95 +72,71 @@ export async function seedDb(): Promise<void> {
             gender: rawGender === "Male" ? Gender.MALE : Gender.FEMALE,
             dateOfBirth: faker.date.between("1920-01-01T00:00:00.000Z", "2000-01-01T00:00:00.000Z"),
         };
-        const addressId = faker.datatype.number({ min: 0, max: numAddresses - 1 });
+        const addressId = faker.datatype.number({ min: numAddresses[0], max: numAddresses[1] - 1 });
 
-        // await userRepository.addUser(user, addressId);
+        await userRepository.addUser(user, addressId);
     }
 
-    // // add addresses
-    // for (const address of addresses) {
-    //     await userRepository.addAddress(address);
-    // }
-    //
-    // // add users
-    // for (const { userData, addressId } of users) {
-    //     await userRepository.addUser(
-    //         {
-    //             ...userData,
-    //             password: await bcrypt.hash(userData.password, 10),
-    //         },
-    //         addressId,
-    //     );
-    // }
-    //
-    // // add doctors
-    // await doctorRepository.addDoctor(1);
-    // await doctorRepository.addDoctor(2);
-    //
-    // // add patients
-    // await patientRepository.addPatient(3);
-    // await patientRepository.addPatient(4);
-    // await patientRepository.addPatient(5);
-    //
-    // // add admin
-    // await adminRepository.addAdmin(6);
-    //
-    // // add health official
-    // await healthOfficialRepository.addHealthOfficial(7);
-    //
-    // // assign patient to doctor
-    // await patientRepository.updateAssignedDoctor(3, 1);
-    // await patientRepository.updateAssignedDoctor(5, 2);
-    //
-    // // add status fields
-    // await statusRepository.updateStatusFields(3, {
-    //     temperature: true,
-    //     weight: true,
-    //     fever: true,
-    //     cough: false,
-    //     shortnessOfBreath: false,
-    //     lossOfTasteAndSmell: true,
-    //     nausea: false,
-    //     stomachAches: false,
-    //     vomiting: false,
-    //     headache: false,
-    //     musclePain: false,
-    //     soreThroat: false,
-    //     otherSymptoms: true,
-    // });
-    //
-    // await statusRepository.updateStatusFields(5, {
-    //     temperature: true,
-    //     weight: true,
-    //     fever: true,
-    //     cough: false,
-    //     shortnessOfBreath: false,
-    //     lossOfTasteAndSmell: true,
-    //     nausea: false,
-    //     stomachAches: false,
-    //     vomiting: false,
-    //     headache: false,
-    //     musclePain: false,
-    //     soreThroat: false,
-    //     otherSymptoms: true,
-    // });
-    //
-    // // add status reports
-    // await statusRepository.insertStatus(3, {
-    //     weight: 150,
-    //     temperature: 29,
-    //     fever: true,
-    //     cough: false,
-    //     shortnessOfBreath: false,
-    //     lossOfTasteAndSmell: true,
-    //     nausea: false,
-    //     stomachAches: false,
-    //     vomiting: false,
-    //     headache: false,
-    //     musclePain: false,
-    //     soreThroat: false,
-    //     otherSymptoms: "No other symptoms",
-    // });
-    //
-    // await pool.end();
+    // assign roles
+    for (let i = numPatients[0]; i < numPatients[1]; i++) {
+        await patientRepository.addPatient(i);
+    }
+    for (let i = numDoctors[0]; i < numDoctors[1]; i++) {
+        await doctorRepository.addDoctor(i);
+    }
+    for (let i = numAdmins[0]; i < numAdmins[1]; i++) {
+        await adminRepository.addAdmin(i);
+    }
+    for (let i = numHealthOfficials[0]; i < numHealthOfficials[1]; i++) {
+        await healthOfficialRepository.addHealthOfficial(i);
+    }
+    for (let i = numImmigrationOfficers[0]; i < numImmigrationOfficers[1]; i++) {
+        await immigrationOfficerRepository.addImmigrationOfficer(i);
+    }
+
+    // assign doctors to patients
+    for (let i = numPatients[0]; i < numPatients[1]; i++) {
+        const doctorId = faker.datatype.number({ min: numDoctors[0], max: numDoctors[1] - 1 });
+        await patientRepository.updateAssignedDoctor(i, doctorId);
+    }
+
+    // assign status fields
+    for (let i = numPatients[0]; i < numPatients[1]; i++) {
+        const fields = {
+            temperature: true,
+            weight: true,
+            fever: faker.datatype.boolean(),
+            cough: faker.datatype.boolean(),
+            shortnessOfBreath: faker.datatype.boolean(),
+            lossOfTasteAndSmell: faker.datatype.boolean(),
+            nausea: faker.datatype.boolean(),
+            stomachAches: faker.datatype.boolean(),
+            vomiting: faker.datatype.boolean(),
+            headache: faker.datatype.boolean(),
+            musclePain: faker.datatype.boolean(),
+            soreThroat: faker.datatype.boolean(),
+            otherSymptoms: true,
+        };
+        await statusRepository.updateStatusFields(i, fields);
+
+        // add status reports
+        const numStatusesPerPatient = 10;
+        for (let j = 0; j < numStatusesPerPatient; j++) {
+            const status = Object.keys(fields)
+                .filter((key) => fields[key])
+                .reduce((obj, key) => {
+                    obj[key] = faker.datatype.boolean();
+                    return obj;
+                }, {});
+
+            await statusRepository.insertStatus(i, {
+                ...status,
+                weight: faker.datatype.number({ min: 120, max: 200, precision: 0.1 }),
+                temperature: faker.datatype.number({ min: 35, max: 41, precision: 0.1 }),
+                otherSymptoms: sampleSymptoms[faker.datatype.number({ min: 0, max: sampleSymptoms.length - 1 })],
+            });
+        }
+    }
+
+    await pool.end();
 }
