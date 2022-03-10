5ce83f77d572e845a924d0634b5c359263ed6a4a
Rafi <rafistep98@gmail.com>
2022-03-10 02:00:03 -0500

    Create table view for patient statuses

--- client/src/views/pages/patient/StatusReports.js
+++ client/src/views/pages/patient/StatusReports.js
@@ -1,22 +1,11 @@
+import BreadCrumbsPage from "@components/breadcrumbs";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import {
-    // DropdownItem,
-    // DropdownMenu,
-    // DropdownToggle,
-    // Table,
-    // UncontrolledDropdown,
-    Card,
-    CardHeader,
-    CardTitle,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    UncontrolledDropdown,
-} from "reactstrap";
-import { Archive, ChevronDown, Edit, Eye, FileText, MoreVertical, Trash } from "react-feather";
+import { Card, DropdownToggle, UncontrolledDropdown } from "reactstrap";
+import { ChevronDown, Eye } from "react-feather";
 import DataTable from "react-data-table-component";
+import { Link } from "react-router-dom";
 
 async function getStatusReports(patientId, token) {
     const res = await axios.get(`http://localhost:8080/statuses/patients/${patientId}`, {
@@ -70,27 +59,13 @@ function StatusReports() {
         {
             name: "Actions",
             allowOverflow: true,
-            cell: () => {
+            cell: (index) => {
                 return (
                     <div className="d-flex">
                         <UncontrolledDropdown>
-                            <DropdownToggle className="pe-1" tag="span">
+                            <DropdownToggle href={"/statuses/" + index.statusId} className="pe-1" tag="a">
                                 <Eye size={18} />
                             </DropdownToggle>
-                            <DropdownMenu end>
-                                <DropdownItem tag="a" href="/" className="w-100" onClick={(e) => e.preventDefault()}>
-                                    <FileText size={15} />
-                                    <span className="align-middle ms-50">Details</span>
-                                </DropdownItem>
-                                <DropdownItem tag="a" href="/" className="w-100" onClick={(e) => e.preventDefault()}>
-                                    <Archive size={15} />
-                                    <span className="align-middle ms-50">Archive</span>
-                                </DropdownItem>
-                                <DropdownItem tag="a" href="/" className="w-100" onClick={(e) => e.preventDefault()}>
-                                    <Trash size={15} />
-                                    <span className="align-middle ms-50">Delete</span>
-                                </DropdownItem>
-                            </DropdownMenu>
                         </UncontrolledDropdown>
                     </div>
                 );
@@ -111,97 +86,26 @@ function StatusReports() {
         return <div></div>;
     }
     return (
-        <Card className="overflow-hidden">
-            <CardHeader>
-                <CardTitle tag="h4">Zero Configuration</CardTitle>
-            </CardHeader>
-            <div className="react-dataTable">
-                <DataTable
-                    noHeader
-                    pagination
-                    data={statusReports}
-                    columns={columns}
-                    className="react-dataTable"
-                    sortIcon={<ChevronDown size={10} />}
-                    paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
-                />
-            </div>
-        </Card>
-
-        /**
         <div>
-            <Card className="card-company-table">
-                <div style={{ margin: "10px" }}>
-                    <div style={{ display: "inline-block", alignSelf: "center" }}>
-                        <p>Show</p>
-                    </div>
-                    <div style={{ display: "inline-block", marginLeft: "15px", justifySelf: "center" }}>
-                        <UncontrolledDropdown style={{ borderWidth: "3px" }}>
-                            <DropdownToggle outline color="secondary" caret>
-                                10
-                            </DropdownToggle>
-                            <DropdownMenu style={{ width: "100%" }}>
-                                <DropdownItem style={{ width: "100%" }} onClick={(e) => e.preventDefault()}>
-                                    10
-                                </DropdownItem>
-                                <DropdownItem style={{ width: "100%" }} onClick={(e) => e.preventDefault()}>
-                                    25
-                                </DropdownItem>
-                                <DropdownItem style={{ width: "100%" }} onClick={(e) => e.preventDefault()}>
-                                    50
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </div>
+            <BreadCrumbsPage
+                breadCrumbTitle="Status Reports"
+                breadCrumbParent="Patient"
+                breadCrumbActive="Status Reports"
+            />
+            <Card className="overflow-hidden">
+                <div className="react-dataTable">
+                    <DataTable
+                        noHeader
+                        pagination
+                        data={statusReports}
+                        columns={columns}
+                        className="react-dataTable"
+                        sortIcon={<ChevronDown size={10} />}
+                        paginationRowsPerPageOptions={[5, 10, 25, 50, 100]}
+                    />
                 </div>
-                <Table bordered striped responsive>
-                    <thead>
-                        <tr>
-                            <th className="sortable">#</th>
-                            <th>Last Updated</th>
-                            <th>Weight</th>
-                            <th>Temperature</th>
-                            <th>Symptoms</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {statusReports.map((object) => (
-                            <tr>
-                                <td>{object.statusId}</td>
-                                <td>{object.createdOn}</td>
-                                <td>{object.statusBody.weight}</td>
-                                <td>{object.statusBody.temperature}</td>
-                                <td>{object.statusBody.otherSymptoms}</td>
-                                <td>
-                                    <UncontrolledDropdown>
-                                        <DropdownToggle
-                                            className="icon-btn hide-arrow"
-                                            color="transparent"
-                                            size="md"
-                                            caret
-                                        >
-                                            <Eye size={19} />
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem href="/" onClick={(e) => e.preventDefault()}>
-                                                <Edit className="me-50" size={15} />
-                                                <span className="align-middle">Edit</span>
-                                            </DropdownItem>
-                                            <DropdownItem href="/" onClick={(e) => e.preventDefault()}>
-                                                <Trash className="me-50" size={15} />{" "}
-                                                <span className="align-middle">Delete</span>
-                                            </DropdownItem>
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
             </Card>
         </div>
-         **/
     );
 }
 
