bf973887bb14223195fec23ff16efc8ce35545e0
Jason Gerard <jasongerard321@gmail.com>
2022-03-20 20:11:24 -0400

    add db schema

--- server/db/init_db.ts
+++ server/db/init_db.ts
@@ -1,13 +1,13 @@
 import "dotenv/config";
 import { Pool } from "pg";
 import { readFileSync } from "fs";
-import { schemas } from "./schemas";
+import { SCHEMAS } from "./schemas";
 
 (async () => {
     const pool = new Pool();
 
     const baseDir = `${__dirname}/schemas`;
-    const files = schemas.map((schema) => `${baseDir}/${schema}.sql`);
+    const files = SCHEMAS.map((schema) => `${baseDir}/${schema}.sql`);
 
     await pool.query("BEGIN;");
 
