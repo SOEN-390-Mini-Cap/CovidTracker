80213a357de8c329a838452d0a376fed452e31b6
Jason Gerard <jasongerard321@gmail.com>
2022-03-23 20:29:51 -0400

    cleanup sidebar

--- client/src/views/pages/chat/SidebarLeft.js
+++ client/src/views/pages/chat/SidebarLeft.js
@@ -1,39 +1,25 @@
-// ** React Imports
 import { useState, useEffect } from "react";
-
-// ** Custom Components
 import Avatar from "@components/avatar";
-
-// ** Store & Actions
 import { selectChat } from "./store";
 import { useDispatch } from "react-redux";
-
-// ** Utils
 import { formatDateToMonthShort, isObjEmpty } from "@utils";
-
-// ** Third Party Components
 import classnames from "classnames";
 import PerfectScrollbar from "react-perfect-scrollbar";
-import { X, Search, CheckSquare, Bell, User, Trash } from "react-feather";
-
-// ** Reactstrap Imports
-import { CardText, InputGroup, InputGroupText, Badge, Input, Button, Label } from "reactstrap";
+import { X, Search } from "react-feather";
+import { CardText, InputGroup, InputGroupText, Badge, Input } from "reactstrap";
 
 const SidebarLeft = (props) => {
     // ** Props & Store
-    const { store, sidebar, handleSidebar, userSidebarLeft, handleUserSidebarLeft } = props;
-    const { chats, contacts, userProfile } = store;
+    const { store, sidebar, handleSidebar } = props;
+    const { chats } = store;
 
     // ** Dispatch
     const dispatch = useDispatch();
 
     // ** State
     const [query, setQuery] = useState("");
-    const [about, setAbout] = useState("");
     const [active, setActive] = useState(0);
-    const [status, setStatus] = useState("online");
     const [filteredChat, setFilteredChat] = useState([]);
-    const [filteredContacts, setFilteredContacts] = useState([]);
 
     // ** Handles User Chat Click
     const handleUserClick = (id) => {
@@ -105,50 +91,11 @@ const SidebarLeft = (props) => {
         }
     };
 
-    // ** Renders Contact
-    const renderContacts = () => {
-        if (contacts && contacts.length) {
-            if (query.length && !filteredContacts.length) {
-                return (
-                    <li className="no-results show">
-                        <h6 className="mb-0">No Chats Found</h6>
-                    </li>
-                );
-            } else {
-                const arrToMap = query.length && filteredContacts.length ? filteredContacts : contacts;
-                return arrToMap.map((item) => {
-                    return (
-                        <li key={item.fullName} onClick={() => handleUserClick(item.id)}>
-                            <Avatar img={item.avatar} imgHeight="42" imgWidth="42" />
-                            <div className="chat-info flex-grow-1">
-                                <h5 className="mb-0">{item.fullName}</h5>
-                                <CardText className="text-truncate">{item.about}</CardText>
-                            </div>
-                        </li>
-                    );
-                });
-            }
-        } else {
-            return null;
-        }
-    };
-
-    // ** Handles Filter
     const handleFilter = (e) => {
         setQuery(e.target.value);
         const searchFilterFunction = (contact) => contact.fullName.toLowerCase().includes(e.target.value.toLowerCase());
         const filteredChatsArr = chats.filter(searchFilterFunction);
-        const filteredContactssArr = contacts.filter(searchFilterFunction);
         setFilteredChat([...filteredChatsArr]);
-        setFilteredContacts([...filteredContactssArr]);
-    };
-
-    const renderAboutCount = () => {
-        if (userProfile && userProfile.about && userProfile.about.length && about.length === 0) {
-            return userProfile.about.length;
-        } else {
-            return about.length;
-        }
     };
 
     return store ? (
