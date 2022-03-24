220cf2b2fd6cf625f70b79bdce7680ca4000a66e
Jason Gerard <jasongerard321@gmail.com>
2022-03-24 12:46:26 -0400

    hide menu items for roles without access

--- client/src/views/pages/patient/PatientList.js
+++ client/src/views/pages/patient/PatientList.js
@@ -8,98 +8,6 @@ import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import { getPatients, putPatientPrioritized } from "../../../services/api";
 
-const columns = (setPatients) => [
-    {
-        name: "ID",
-        sortable: true,
-        width: "80px",
-        selector: (row) => row.account.userId,
-    },
-    {
-        name: "Name",
-        sortable: true,
-        selector: (row) => (
-            <Fragment>
-                <span className="fw-bold">
-                    {row.firstName} {row.lastName}
-                </span>
-                <br /> {row.account.email}
-            </Fragment>
-        ),
-    },
-    {
-        name: "Address",
-        sortable: true,
-        selector: (row) => (
-            <Fragment>
-                {row.address.streetAddress}
-                <br />
-                {row.address.city}, {row.address.province} {row.address.postalCode}
-                <br />
-                {row.address.country}
-            </Fragment>
-        ),
-    },
-    {
-        name: "Date of Birth",
-        sortable: true,
-        width: "140px",
-        selector: (row) =>
-            new Date(row.dateOfBirth).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-            }),
-    },
-    {
-        name: "Gender",
-        sortable: true,
-        width: "100px",
-        selector: (row) => (
-            <Badge pill color={row.gender === "MALE" ? "light-info" : "light-danger"}>
-                {row.gender}
-            </Badge>
-        ),
-    },
-    {
-        name: "Phone",
-        sortable: true,
-        width: "140px",
-        selector: (row) => row.phoneNumber,
-    },
-    {
-        name: "Actions",
-        allowOverflow: true,
-        width: "80px",
-        cell: (row) => {
-            return (
-                <div className="d-flex">
-                    <PrioritizeFlag row={row} setPatients={setPatients} />
-                    <UncontrolledDropdown>
-                        <DropdownToggle tag="span">
-                            <MoreVertical color="#5E5873" size={20} />
-                        </DropdownToggle>
-                        <DropdownMenu end>
-                            <DropdownItem tag={Link} to={`/add_test/patients/${row.account.userId}`} className="w-100">
-                                Add Test Result
-                            </DropdownItem>
-                            <DropdownItem tag={Link} to={`/tests/patients/${row.account.userId}`} className="w-100">
-                                Test Results
-                            </DropdownItem>
-                            <DropdownItem tag={Link} to={`/statuses/patients/${row.account.userId}`} className="w-100">
-                                Status Reports
-                            </DropdownItem>
-                            <DropdownItem tag={Link} to={`/create_appointment/${row.account.userId}`} className="w-100">
-                                Book Appointment
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                </div>
-            );
-        },
-    },
-];
-
 function PrioritizeFlag({ row, setPatients }) {
     const token = useSelector(selectToken);
 
@@ -121,6 +29,8 @@ const selectToken = (state) => state.auth.userData.token;
 
 function PatientList() {
     const token = useSelector(selectToken);
+    const role = useSelector((state) => state.auth.userData.user.role);
+
     const [patients, setPatients] = useState(null);
     useEffect(() => {
         async function f() {
@@ -129,6 +39,116 @@ function PatientList() {
         }
         f();
     }, [token]);
+
+    const columns = [
+        {
+            name: "ID",
+            sortable: true,
+            width: "80px",
+            selector: (row) => row.account.userId,
+        },
+        {
+            name: "Name",
+            sortable: true,
+            selector: (row) => (
+                <Fragment>
+                    <span className="fw-bold">
+                        {row.firstName} {row.lastName}
+                    </span>
+                    <br /> {row.account.email}
+                </Fragment>
+            ),
+        },
+        {
+            name: "Address",
+            sortable: true,
+            selector: (row) => (
+                <Fragment>
+                    {row.address.streetAddress}
+                    <br />
+                    {row.address.city}, {row.address.province} {row.address.postalCode}
+                    <br />
+                    {row.address.country}
+                </Fragment>
+            ),
+        },
+        {
+            name: "Date of Birth",
+            sortable: true,
+            width: "140px",
+            selector: (row) =>
+                new Date(row.dateOfBirth).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                }),
+        },
+        {
+            name: "Gender",
+            sortable: true,
+            width: "100px",
+            selector: (row) => (
+                <Badge pill color={row.gender === "MALE" ? "light-info" : "light-danger"}>
+                    {row.gender}
+                </Badge>
+            ),
+        },
+        {
+            name: "Phone",
+            sortable: true,
+            width: "140px",
+            selector: (row) => row.phoneNumber,
+        },
+        {
+            name: "Actions",
+            allowOverflow: true,
+            width: "80px",
+            cell: (row) => {
+                return (
+                    <div className="d-flex">
+                        <PrioritizeFlag row={row} setPatients={setPatients} />
+                        <UncontrolledDropdown>
+                            <DropdownToggle tag="span">
+                                <MoreVertical color="#5E5873" size={20} />
+                            </DropdownToggle>
+                            <DropdownMenu end>
+                                {(role === "DOCTOR" || role === "HEALTH_OFFICIAL") && (
+                                    <DropdownItem
+                                        tag={Link}
+                                        to={`/add_test/patients/${row.account.userId}`}
+                                        className="w-100"
+                                    >
+                                        Add Test Result
+                                    </DropdownItem>
+                                )}
+                                <DropdownItem tag={Link} to={`/tests/patients/${row.account.userId}`} className="w-100">
+                                    Test Results
+                                </DropdownItem>
+                                {(role === "DOCTOR" || role === "HEALTH_OFFICIAL") && (
+                                    <DropdownItem
+                                        tag={Link}
+                                        to={`/statuses/patients/${row.account.userId}`}
+                                        className="w-100"
+                                    >
+                                        Status Reports
+                                    </DropdownItem>
+                                )}
+                                {role === "DOCTOR" && (
+                                    <DropdownItem
+                                        tag={Link}
+                                        to={`/create_appointment/${row.account.userId}`}
+                                        className="w-100"
+                                    >
+                                        Book Appointment
+                                    </DropdownItem>
+                                )}
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </div>
+                );
+            },
+        },
+    ];
 
     return (
         <div>
@@ -144,7 +164,7 @@ function PatientList() {
                             noHeader
                             pagination
                             data={patients}
-                            columns={columns(setPatients)}
+                            columns={columns}
                             className="react-dataTable"
                             sortIcon={<ChevronDown size={10} />}
                             paginationRowsPerPageOptions={[10, 25, 50, 100]}
