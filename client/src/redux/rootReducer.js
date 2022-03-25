a006613980331c4933b1add2b2e83016a7dfcf69
Jason Gerard <jasongerard321@gmail.com>
2022-03-24 21:19:35 -0400

    remove extra files

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -2,18 +2,12 @@
 import navbar from "./navbar";
 import layout from "./layout";
 import auth from "./authentication";
-import users from "@src/views/apps/user/store";
-import dataTables from "@src/views/tables/data-tables/store";
-import permissions from "@src/views/apps/roles-permissions/store";
 import chat from "@src/views/pages/chat/store";
 
 const rootReducer = {
     auth,
-    users,
     navbar,
     layout,
-    dataTables,
-    permissions,
     chat,
 };
 
