b37cdc5fb43e8ce8fbe4d6053c437a0b49b4d595
Andre <psnmeee@gmail.com>
2022-04-04 13:12:52 -0400

    added dashboard unit tests

---
+++ server/tests/unit/controllers/dashboard_controller.ts
@@ -0,0 +1,48 @@
+import { expect } from "chai";
+import { DashboardController } from "../../../src/controllers/dashboard_controller";
+import { createSandbox, SinonStub } from "sinon";
+import { DashboardStrategy } from "../../../src/services/dashboard/dashboard_strategy";
+
+describe("dashboard_controller.ts", () => {
+    const dashboardStrategy: any = {
+        dashboardStrategyFactory: Function,
+    };
+    const controller = new DashboardController(dashboardStrategy);
+
+    let dashboardStrategyFactoryStub: SinonStub;
+
+    const sandbox = createSandbox();
+    const res: any = {
+        json: Function,
+    };
+
+    let req: any;
+    let resJsonStub: SinonStub;
+
+    beforeEach(() => {
+        sandbox.restore();
+        resJsonStub = sandbox.stub(res, "json");
+
+        dashboardStrategyFactoryStub = sandbox.stub(dashboardStrategy, "dashboardStrategyFactory");
+        req = {};
+    });
+
+    describe("DashboardController::getDashboards", () => {
+        it("should call res.json with 200", async () => {
+            dashboardStrategyFactoryStub.returns(() => {
+                return null;
+            });
+            await (controller as any).getDashboards(req, res);
+            expect(resJsonStub.calledWith(200)).to.be.true;
+        });
+
+        describe("DashboardController::getDashboards", () => {
+            it("should call res.json with 200", async () => {
+                dashboardStrategyFactoryStub.rejects(new Error());
+                await (controller as any).getDashboards(req, res);
+                expect(resJsonStub.callCount).to.equal(1);
+                expect(resJsonStub.calledWith(500)).to.be.true;
+            });
+        });
+    });
+});
